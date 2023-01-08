d8c329761cd53b18e757dbf32b3f84e6469f6b7b
AruSeito <weichen@illasoft.com>
2023-01-08 21:53:11 +0800

    feat: user role utils

--- UserRoleUtils/index.ts
+++ UserRoleUtils/index.ts
@@ -1,4 +1,12 @@
-import { USER_ROLE } from "./interface"
+import {
+  ACTION_ACCESS,
+  ACTION_DELETE,
+  ACTION_MANAGE,
+  ACTION_SPECIAL,
+  ATTRIBUTE_CATEGORY,
+  ATTRIBUTE_GROUP,
+  USER_ROLE,
+} from "./interface"
 
 export const USER_ROLE_ARRAY = [
   USER_ROLE.OWNER,
@@ -73,4 +81,340 @@ export const isBiggerThenTargetRole = (
   return isEqual
     ? currentUserRoleIndex <= targetRoleIndex
     : currentUserRoleIndex < targetRoleIndex
+}
+
+interface AttributeConfigList {
+  [key: string]: {
+    [key: string]: {
+      [key: string]: {
+        [key: string]: boolean
+      }
+    }
+  }
+}
+
+export const attributeConfigList: AttributeConfigList = {
+  [ATTRIBUTE_CATEGORY.ACCESS]: {
+    [USER_ROLE.OWNER]: {
+      [ATTRIBUTE_GROUP.TEAM]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.INVITE]: {
+        [ACTION_ACCESS.VIEW]: true,
+        [ACTION_ACCESS.INVITE_BY_LINK]: true,
+        [ACTION_ACCESS.INVITE_BY_EMAIL]: true,
+        [ACTION_ACCESS.INVITE_ADMIN]: true,
+        [ACTION_ACCESS.INVITE_EDITOR]: true,
+        [ACTION_ACCESS.INVITE_VIEWER]: true,
+      },
+      [ATTRIBUTE_GROUP.DOMAIN]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.BILLING]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.RESOURCE]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.HUB]: { [ACTION_ACCESS.VIEW]: true },
+    },
+    [USER_ROLE.ADMIN]: {
+      [ATTRIBUTE_GROUP.TEAM]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.INVITE]: {
+        [ACTION_ACCESS.VIEW]: true,
+        [ACTION_ACCESS.INVITE_BY_LINK]: true,
+        [ACTION_ACCESS.INVITE_BY_EMAIL]: true,
+        [ACTION_ACCESS.INVITE_ADMIN]: true,
+        [ACTION_ACCESS.INVITE_EDITOR]: true,
+        [ACTION_ACCESS.INVITE_VIEWER]: true,
+      },
+      [ATTRIBUTE_GROUP.DOMAIN]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.RESOURCE]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.HUB]: { [ACTION_ACCESS.VIEW]: true },
+    },
+    [USER_ROLE.EDITOR]: {
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.INVITE]: {
+        [ACTION_ACCESS.VIEW]: true,
+        [ACTION_ACCESS.INVITE_BY_LINK]: true,
+        [ACTION_ACCESS.INVITE_BY_EMAIL]: true,
+        [ACTION_ACCESS.INVITE_EDITOR]: true,
+        [ACTION_ACCESS.INVITE_VIEWER]: true,
+      },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.RESOURCE]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.HUB]: { [ACTION_ACCESS.VIEW]: true },
+    },
+    [USER_ROLE.VIEWER]: {
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_ACCESS.VIEW]: true },
+      [ATTRIBUTE_GROUP.INVITE]: {
+        [ACTION_ACCESS.VIEW]: true,
+        [ACTION_ACCESS.INVITE_BY_LINK]: true,
+        [ACTION_ACCESS.INVITE_BY_EMAIL]: true,
+        [ACTION_ACCESS.INVITE_VIEWER]: true,
+      },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_ACCESS.VIEW]: true },
+    },
+  },
+  [ATTRIBUTE_CATEGORY.DELETE]: {
+    [USER_ROLE.OWNER]: {
+      [ATTRIBUTE_GROUP.TEAM]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.INVITE]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.DOMAIN]: {
+        [ACTION_DELETE.TEAM_DOMAIN]: true,
+        [ACTION_DELETE.APP_DOMAIN]: true,
+      },
+      [ATTRIBUTE_GROUP.BILLING]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.RESOURCE]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.HUB]: { [ACTION_DELETE.DELETE]: true },
+    },
+    [USER_ROLE.ADMIN]: {
+      [ATTRIBUTE_GROUP.TEAM]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.INVITE]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.DOMAIN]: {
+        [ACTION_DELETE.TEAM_DOMAIN]: true,
+        [ACTION_DELETE.APP_DOMAIN]: true,
+      },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.RESOURCE]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.HUB]: { [ACTION_DELETE.DELETE]: true },
+    },
+    [USER_ROLE.EDITOR]: {
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.APP]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.RESOURCE]: { [ACTION_DELETE.DELETE]: true },
+      [ATTRIBUTE_GROUP.HUB]: { [ACTION_DELETE.DELETE]: true },
+    },
+    [USER_ROLE.VIEWER]: {
+      [ATTRIBUTE_GROUP.USER]: { [ACTION_DELETE.DELETE]: true },
+    },
+  },
+  [ATTRIBUTE_CATEGORY.MANAGE]: {
+    [USER_ROLE.OWNER]: {
+      [ATTRIBUTE_GROUP.TEAM]: {
+        [ACTION_MANAGE.TEAM_NAME]: true,
+        [ACTION_MANAGE.TEAM_ICON]: true,
+        [ACTION_MANAGE.UPDATE_TEAM_DOMAIN]: true,
+        [ACTION_MANAGE.TEAM_CONFIG]: true,
+      },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: {
+        [ACTION_MANAGE.REMOVE_MEMBER]: true,
+        [ACTION_MANAGE.ROLE]: true,
+        [ACTION_MANAGE.ROLE_FROM_OWNER]: true,
+        [ACTION_MANAGE.ROLE_FROM_ADMIN]: true,
+        [ACTION_MANAGE.ROLE_FROM_EDITOR]: true,
+        [ACTION_MANAGE.ROLE_FROM_VIEWER]: true,
+        [ACTION_MANAGE.ROLE_TO_OWNER]: true,
+        [ACTION_MANAGE.ROLE_TO_ADMIN]: true,
+        [ACTION_MANAGE.ROLE_TO_EDITOR]: true,
+        [ACTION_MANAGE.ROLE_TO_VIEWER]: true,
+      },
+      [ATTRIBUTE_GROUP.USER]: {
+        [ACTION_MANAGE.RENAME_USER]: true,
+        [ACTION_MANAGE.UPDATE_USER_AVATAR]: true,
+      },
+      [ATTRIBUTE_GROUP.INVITE]: {
+        [ACTION_MANAGE.CONFIG_INVITE]: true,
+        [ACTION_MANAGE.INVITE_LINK]: true,
+      },
+      [ATTRIBUTE_GROUP.DOMAIN]: {
+        [ACTION_MANAGE.TEAM_DOMAIN]: true,
+        [ACTION_MANAGE.APP_DOMAIN]: true,
+      },
+      [ATTRIBUTE_GROUP.BILLING]: { [ACTION_MANAGE.PAYMENT_INFO]: true },
+      [ATTRIBUTE_GROUP.APP]: {
+        [ACTION_MANAGE.CREATE_APP]: true,
+        [ACTION_MANAGE.EDIT_APP]: true,
+      },
+      [ATTRIBUTE_GROUP.RESOURCE]: {
+        [ACTION_MANAGE.CREATE_RESOURCE]: true,
+        [ACTION_MANAGE.EDIT_RESOURCE]: true,
+      },
+      [ATTRIBUTE_GROUP.HUB]: {},
+    },
+    [USER_ROLE.ADMIN]: {
+      [ATTRIBUTE_GROUP.TEAM]: {
+        [ACTION_MANAGE.TEAM_NAME]: true,
+        [ACTION_MANAGE.TEAM_ICON]: true,
+        [ACTION_MANAGE.UPDATE_TEAM_DOMAIN]: true,
+        [ACTION_MANAGE.TEAM_CONFIG]: true,
+      },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: {
+        [ACTION_MANAGE.REMOVE_MEMBER]: true,
+        [ACTION_MANAGE.ROLE]: true,
+        [ACTION_MANAGE.ROLE_FROM_ADMIN]: true,
+        [ACTION_MANAGE.ROLE_FROM_EDITOR]: true,
+        [ACTION_MANAGE.ROLE_FROM_VIEWER]: true,
+        [ACTION_MANAGE.ROLE_TO_ADMIN]: true,
+        [ACTION_MANAGE.ROLE_TO_EDITOR]: true,
+        [ACTION_MANAGE.ROLE_TO_VIEWER]: true,
+      },
+      [ATTRIBUTE_GROUP.USER]: {
+        [ACTION_MANAGE.RENAME_USER]: true,
+        [ACTION_MANAGE.UPDATE_USER_AVATAR]: true,
+      },
+      [ATTRIBUTE_GROUP.INVITE]: {
+        [ACTION_MANAGE.CONFIG_INVITE]: true,
+        [ACTION_MANAGE.INVITE_LINK]: true,
+      },
+      [ATTRIBUTE_GROUP.DOMAIN]: {
+        [ACTION_MANAGE.TEAM_DOMAIN]: true,
+        [ACTION_MANAGE.APP_DOMAIN]: true,
+      },
+      [ATTRIBUTE_GROUP.APP]: {
+        [ACTION_MANAGE.CREATE_APP]: true,
+        [ACTION_MANAGE.EDIT_APP]: true,
+      },
+      [ATTRIBUTE_GROUP.RESOURCE]: {
+        [ACTION_MANAGE.CREATE_RESOURCE]: true,
+        [ACTION_MANAGE.EDIT_RESOURCE]: true,
+      },
+      [ATTRIBUTE_GROUP.HUB]: {},
+    },
+    [USER_ROLE.EDITOR]: {
+      [ATTRIBUTE_GROUP.USER]: {
+        [ACTION_MANAGE.RENAME_USER]: true,
+        [ACTION_MANAGE.UPDATE_USER_AVATAR]: true,
+      },
+      [ATTRIBUTE_GROUP.APP]: {
+        [ACTION_MANAGE.CREATE_APP]: true,
+        [ACTION_MANAGE.EDIT_APP]: true,
+      },
+      [ATTRIBUTE_GROUP.RESOURCE]: {
+        [ACTION_MANAGE.CREATE_RESOURCE]: true,
+        [ACTION_MANAGE.EDIT_RESOURCE]: true,
+      },
+      [ATTRIBUTE_GROUP.HUB]: {},
+    },
+    [USER_ROLE.VIEWER]: {
+      [ATTRIBUTE_GROUP.USER]: {
+        [ACTION_MANAGE.RENAME_USER]: true,
+        [ACTION_MANAGE.UPDATE_USER_AVATAR]: true,
+      },
+      [ATTRIBUTE_GROUP.HUB]: {},
+    },
+  },
+  [ATTRIBUTE_CATEGORY.SPECIAL]: {
+    [USER_ROLE.OWNER]: {
+      [ATTRIBUTE_GROUP.TEAM]: {
+        [ACTION_SPECIAL.EDITOR_AND_VIEWER_CAN_INVITE_BY_LINK_SW]: true,
+      },
+      [ATTRIBUTE_GROUP.TEAM_MEMBER]: { [ACTION_SPECIAL.TRANSFER_OWNER]: true },
+      [ATTRIBUTE_GROUP.INVITE]: { [ACTION_SPECIAL.INVITE_LINK_RENEW]: true },
+    },
+    [USER_ROLE.ADMIN]: {
+      [ATTRIBUTE_GROUP.TEAM]: {
+        [ACTION_SPECIAL.EDITOR_AND_VIEWER_CAN_INVITE_BY_LINK_SW]: true,
+      },
+      [ATTRIBUTE_GROUP.INVITE]: { [ACTION_SPECIAL.INVITE_LINK_RENEW]: true },
+    },
+    [USER_ROLE.EDITOR]: {},
+    [USER_ROLE.VIEWER]: {},
+  },
+}
+
+interface InviteRoleAttributeMap {
+  [key: string]: number
+}
+
+export const inviteRoleAttributeMap: InviteRoleAttributeMap = {
+  [USER_ROLE.OWNER]: ACTION_ACCESS.INVITE_OWNER,
+  [USER_ROLE.ADMIN]: ACTION_ACCESS.INVITE_ADMIN,
+  [USER_ROLE.EDITOR]: ACTION_ACCESS.INVITE_EDITOR,
+  [USER_ROLE.VIEWER]: ACTION_ACCESS.INVITE_VIEWER,
+}
+
+// this config map target role to target manage user role attribute
+// e.g. you want to modify a user to role USER_ROLE_EDITOR, so it's mapped attribute is ACTION_MANAGE_ROLE_TO_EDITOR
+interface ModifyRoleFromAttributeMap {
+  [key: string]: number
+}
+const modifyRoleFromAttributeMap: ModifyRoleFromAttributeMap = {
+  [USER_ROLE.OWNER]: ACTION_MANAGE.ROLE_FROM_OWNER,
+  [USER_ROLE.ADMIN]: ACTION_MANAGE.ROLE_FROM_ADMIN,
+  [USER_ROLE.EDITOR]: ACTION_MANAGE.ROLE_FROM_EDITOR,
+  [USER_ROLE.VIEWER]: ACTION_MANAGE.ROLE_FROM_VIEWER,
+}
+interface ModifyRoleToAttributeMap {
+  [key: string]: number
+}
+const modifyRoleToAttributeMap: ModifyRoleToAttributeMap = {
+  [USER_ROLE.OWNER]: ACTION_MANAGE.ROLE_TO_OWNER,
+  [USER_ROLE.ADMIN]: ACTION_MANAGE.ROLE_TO_ADMIN,
+  [USER_ROLE.EDITOR]: ACTION_MANAGE.ROLE_TO_EDITOR,
+  [USER_ROLE.VIEWER]: ACTION_MANAGE.ROLE_TO_VIEWER,
+}
+
+const accessAttribute = attributeConfigList[ATTRIBUTE_CATEGORY.ACCESS]
+const deleteAttribute = attributeConfigList[ATTRIBUTE_CATEGORY.DELETE]
+const manageAttribute = attributeConfigList[ATTRIBUTE_CATEGORY.MANAGE]
+const specialAttribute = attributeConfigList[ATTRIBUTE_CATEGORY.SPECIAL]
+
+export const canAccess = (
+  userRole: USER_ROLE,
+  attributeGroup: ATTRIBUTE_GROUP,
+  attribute: ACTION_ACCESS,
+) => {
+  return !!accessAttribute[userRole]?.[attributeGroup]?.[attribute]
+}
+export const canDelete = (
+  userRole: USER_ROLE,
+  attributeGroup: ATTRIBUTE_GROUP,
+  attribute: ACTION_ACCESS,
+) => {
+  return !!deleteAttribute[userRole]?.[attributeGroup]?.[attribute]
+}
+
+export const canManage = (
+  userRole: USER_ROLE,
+  attributeGroup: ATTRIBUTE_GROUP,
+  attribute: ACTION_ACCESS,
+) => {
+  return !!manageAttribute[userRole]?.[attributeGroup]?.[attribute]
+}
+
+export const canManageSpecial = (
+  userRole: USER_ROLE,
+  attributeGroup: ATTRIBUTE_GROUP,
+  attribute: ACTION_ACCESS,
+) => {
+  return !!specialAttribute[userRole]?.[attributeGroup]?.[attribute]
+}
+
+export const canInvite = (userRole: USER_ROLE) => {
+  const attribute = inviteRoleAttributeMap[userRole]
+  if (!attribute) {
+    return false
+  }
+  return !!accessAttribute[userRole]?.[ATTRIBUTE_GROUP.INVITE]?.[attribute]
+}
+
+// [userRole][group]
+export const canModifyRoleFromTo = (
+  userRole: USER_ROLE,
+  attributeGroup: ATTRIBUTE_GROUP,
+  fromRole: USER_ROLE,
+  toRole: USER_ROLE,
+) => {
+  const fromAttribute = modifyRoleFromAttributeMap[fromRole]
+  const toAttribute = modifyRoleToAttributeMap[toRole]
+  if (!fromAttribute || !toAttribute) {
+    return false
+  }
+  const fromResult =
+    manageAttribute[userRole]?.[attributeGroup]?.[fromAttribute]
+  const toResult = manageAttribute[userRole]?.[attributeGroup]?.[toAttribute]
+  if (fromResult == undefined || toResult == undefined) {
+    return false
+  }
+  return fromResult && toResult
+}
+
+export const doesNowUserAreEditorOrViewer = (userRole: USER_ROLE) => {
+  return userRole === USER_ROLE.EDITOR || userRole === USER_ROLE.VIEWER
 }
--- UserRoleUtils/interface.ts
+++ UserRoleUtils/interface.ts
@@ -1,13 +1,101 @@
 export enum USER_ROLE {
   "GUEST" = -1,
   "OWNER" = 1,
-  "ADMIN" = 2,
-  "EDITOR" = 3,
-  "VIEWER" = 4,
-  "CUSTOM" = 5,
+  "ADMIN",
+  "EDITOR",
+  "VIEWER",
+  "CUSTOM",
 }
+
+export type ValidUserRole = Extract<
+  USER_ROLE,
+  USER_ROLE.OWNER | USER_ROLE.ADMIN | USER_ROLE.EDITOR | USER_ROLE.VIEWER
+>
 
 export enum USER_STATUS {
   "OK" = 1,
   "PENDING" = 2,
+}
+
+export enum ATTRIBUTE_GROUP {
+  TEAM = 1, // cloud team
+  TEAM_MEMBER, // cloud team member
+  USER, // cloud user
+  INVITE, // cloud invite
+  DOMAIN, // cloud domain
+  BILLING, // cloud billing
+  APP, // builder app
+  RESOURCE, // builder resource
+  HUB, // builder hub
+}
+
+export enum ATTRIBUTE_CATEGORY {
+  ACCESS = 1,
+  DELETE,
+  MANAGE,
+  SPECIAL,
+}
+
+export enum ACTION_ACCESS {
+  // Basic Attribute
+  VIEW = 0, // 访问 Attribute
+  // Invite Attribute
+  INVITE_BY_LINK, // 使用链接邀请用户
+  INVITE_BY_EMAIL, // 使用邮件邀请用户
+  INVITE_OWNER,
+  INVITE_ADMIN,
+  INVITE_EDITOR,
+  INVITE_VIEWER,
+}
+
+export enum ACTION_MANAGE {
+  // Team Attribute
+  TEAM_NAME = 0, // 重命名 Team Attribute
+  TEAM_ICON, // 更新 icon
+  TEAM_CONFIG, // 更新 team 设置
+  UPDATE_TEAM_DOMAIN, // 更新 team domain
+  // Team Member Attribute
+  REMOVE_MEMBER, // 移除团队成员的 Attribute
+  ROLE, // 修改团队成员角色的 Attribute
+  ROLE_FROM_OWNER, // 将用户角色修改为 owner
+  ROLE_FROM_ADMIN, // 将用户角色修改为 admin
+  ROLE_FROM_EDITOR, // 将用户角色修改为 editor
+  ROLE_FROM_VIEWER, // 将用户角色修改为 viewer
+  ROLE_TO_OWNER, // 将用户角色修改为 owner
+  ROLE_TO_ADMIN, // 将用户角色修改为 admin
+  ROLE_TO_EDITOR, // 将用户角色修改为 editor
+  ROLE_TO_VIEWER, // 将用户角色修改为 viewer
+  // User Attribute
+  RENAME_USER, // 重命名用户
+  UPDATE_USER_AVATAR, // 更新 avatar
+  // Invite Attribute
+  CONFIG_INVITE, // 配置邀请选项和参数
+  INVITE_LINK, // 配置 invite link
+  // Domain Attribute
+  TEAM_DOMAIN, // 更新 Team Domain
+  APP_DOMAIN, // 更新 App domain
+  // Billing Attribute
+  PAYMENT_INFO, // 编辑付款信息
+  // App Attribute
+  CREATE_APP, // 创建 APP
+  EDIT_APP, // 编辑 APP
+  // Resource Attribute
+  CREATE_RESOURCE, // 创建 Resource
+  EDIT_RESOURCE, // 编辑 Resource
+}
+
+export enum ACTION_DELETE {
+  DELETE = 0, // 删除 Attribute
+  // Domain Attribute
+  TEAM_DOMAIN, // 删除 Team Domain
+  APP_DOMAIN, // 删除 App Domain
+}
+
+export enum ACTION_SPECIAL {
+  // Team Attribute
+  EDITOR_AND_VIEWER_CAN_INVITE_BY_LINK_SW = 0, // editor 和 viewer 可以使用链接邀请的 Attribute
+  // Team Member Attribute
+  TRANSFER_OWNER, // 转移 owner 的 Attribute
+  // Invite Attribute
+  INVITE_LINK_RENEW, // 更新邀请链接
 }
