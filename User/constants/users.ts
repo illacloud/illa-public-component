d0ca0eed6a22508b0aff02b6a08802601c162481
xiaoyu <lavamint@outlook.com>
2023-03-21 18:40:10 +0800

    feat: update REDIRECT_URL

--- User/constants/users.ts
+++ User/constants/users.ts
@@ -2,13 +2,9 @@ export const GITHUB_CLIENT_ID = "171a5a900ac20f970f56"
 
 export const GITHUB_AUTHORIZE_URL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}`
 
-export const LOGIN_REDIRECT_URL = `${location.protocol}//${
+export const OAUTH_REDIRECT_URL = `${location.protocol}//${
   import.meta.env.VITE_CLOUD_URL
-}/login`
-export const REGISTER_REDIRECT_URL = "https://192.168.50.170:5173/register"
-export const CONNECT_REDIRECT_URL = "https://192.168.50.170:5173/register"
-
-export const GITHUB_OAUTH_URL = `${GITHUB_AUTHORIZE_URL}&redirect_uri=${LOGIN_REDIRECT_URL}`
+}/oauth`
 
 export const openOAuthUrl = (url?: string) => {
   window.open(url, "_self")
