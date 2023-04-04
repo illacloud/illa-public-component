2d3393fffaaefba1cff1e52f4940dbcfc4bd2327
AruSeito <weichen@illasoft.com>
2023-04-04 13:37:15 +0800

    Revert "Revert "Merge pull request #2 from illacloud/feat/random_avatar_backgroundColor""
    This reverts commit d16c80b2cb08030ebda6b1cbc0c2062736b95594.

--- Avatar/index.tsx
+++ Avatar/index.tsx
@@ -3,6 +3,7 @@ import {
   applyAvatarStyle,
   avatarImgStyle,
 } from "@/illa-public-component/Avatar/style"
+import { getColorByIndex, getColorIndexByString } from "@/utils/colorHelper"
 
 interface AvatarProps extends HTMLAttributes<HTMLSpanElement> {
   avatarUrl?: string
@@ -10,23 +11,14 @@ interface AvatarProps extends HTMLAttributes<HTMLSpanElement> {
   name?: string
 }
 
-// Generate id from strings
-const stringToColour = (str?: string) => {
-  if (!str) return "#654aec"
-  let hash = 0
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash)
-    hash = hash & hash
-  }
-  return `hsl(${hash % 360}, ${100}%, ${70}%)`
-}
-
 export const Avatar: FC<AvatarProps> = (props) => {
   const { avatarUrl, id, name, className, ...otherProps } = props
 
   const { avatarBgColor, avatarText, emptyStatus } = useMemo(() => {
     return {
-      avatarBgColor: avatarUrl ? "#ffffff" : stringToColour(id),
+      avatarBgColor: avatarUrl
+        ? "#ffffff"
+        : getColorByIndex(getColorIndexByString(id || "")),
       avatarText: name?.substring?.(0, 1).toUpperCase() || "U",
       emptyStatus: !avatarUrl && !name,
     }
