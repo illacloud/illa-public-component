86990ead0f43f6fdf1c3ff155cbe2ffe62a15223
AruSeito <weichen@illasoft.com>
2023-04-10 13:10:14 +0800

    feat: ✨ update deviceUUID's rule

--- MixpanelUtils/index.tsx
+++ MixpanelUtils/index.tsx
@@ -11,31 +11,33 @@ class ILLAMixpanelTools {
   private static instance: ILLAMixpanelTools | null = null
 
   constructor() {
-    const deviceID = getDeviceUUID()
-    mixpanel.init(import.meta.env.ILLA_MIXPANEL_API_KEY, {
-      debug: import.meta.env.DEV,
-      test:
-        import.meta.env.DEV ||
-        import.meta.env.ILLA_BUILDER_ENV !== "production" ||
-        import.meta.env.ILLA_CLOUD_ENV !== "production",
-      ignore_dnt: import.meta.env.DEV,
-      loaded(mixpanelProto) {
-        mixpanelProto.identify(deviceID)
-        const originalTrack = mixpanelProto.track
-        mixpanelProto.track = function (event, properties) {
-          originalTrack.call(mixpanelProto, event, {
-            ...properties,
-            $device_id: deviceID,
-            environment: import.meta.env.DEV
-              ? "development"
-              : import.meta.env.ILLA_BUILDER_ENV ||
-                import.meta.env.ILLA_CLOUD_ENV,
-            browser_language: getBrowserLanguage(),
-            illa_language: getIllaLanguage(),
-          })
-        }
-      },
-    })
+    if (import.meta.env.VITE_INSTANCE_ID === "CLOUD") {
+      const deviceID = getDeviceUUID()
+      mixpanel.init(import.meta.env.ILLA_MIXPANEL_API_KEY, {
+        debug: import.meta.env.DEV,
+        test:
+          import.meta.env.DEV ||
+          import.meta.env.ILLA_BUILDER_ENV !== "production" ||
+          import.meta.env.ILLA_CLOUD_ENV !== "production",
+        ignore_dnt: import.meta.env.DEV,
+        loaded(mixpanelProto) {
+          mixpanelProto.identify(deviceID)
+          const originalTrack = mixpanelProto.track
+          mixpanelProto.track = function (event, properties) {
+            originalTrack.call(mixpanelProto, event, {
+              ...properties,
+              $device_id: deviceID,
+              environment: import.meta.env.DEV
+                ? "development"
+                : import.meta.env.ILLA_BUILDER_ENV ||
+                  import.meta.env.ILLA_CLOUD_ENV,
+              browser_language: getBrowserLanguage(),
+              illa_language: getIllaLanguage(),
+            })
+          }
+        },
+      })
+    }
   }
 
   public static getInstance() {
@@ -47,19 +49,26 @@ class ILLAMixpanelTools {
   }
 
   public track(event: ILLA_MIXPANEL_EVENT_TYPE, properties: ILLAProperties) {
-    mixpanel.track(event, {
-      ...properties,
-    })
+    if (import.meta.env.VITE_INSTANCE_ID === "CLOUD") {
+      mixpanel.track(event, {
+        ...properties,
+      })
+    }
   }
 
   public pageTimeEvent() {
-    mixpanel.time_event("page_duration")
+    if (import.meta.env.VITE_INSTANCE_ID === "CLOUD") {
+      mixpanel.time_event("page_duration")
+    }
   }
 
-  public trackTimeEvent(pageName: ILLA_PAGE_NAME, properties: ILLAProperties) {
-    mixpanel.track("page_duration", {
-      page: pageName,
-    })
+  public trackTimeEvent(pageName: ILLA_PAGE_NAME, teamIdentifier: string) {
+    if (import.meta.env.VITE_INSTANCE_ID === "CLOUD") {
+      mixpanel.track("page_duration", {
+        page: pageName,
+        parameter12: teamIdentifier,
+      })
+    }
   }
 }
 
