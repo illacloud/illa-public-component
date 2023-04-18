b4380ec429c56a7afe9e8468c44945d23e5f4931
xiaoyu-a <lavamint@outlook.com>
2023-04-19 00:35:24 +0800

    fix: invite modal style

--- MemberList/components/Header/style.ts
+++ MemberList/components/Header/style.ts
@@ -46,7 +46,7 @@ export const modalWithMaskWrapperStyle = css`
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 1;
+  z-index: 2;
   display: flex;
   align-items: center;
   justify-content: center;
