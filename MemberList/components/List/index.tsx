d55c01b548d36e3bfbd26a487f4186deeea35f1b
AruSeito <weichen@illasoft.com>
2023-06-07 22:54:28 +0800

    chore: remove member listener

--- MemberList/components/List/index.tsx
+++ MemberList/components/List/index.tsx
@@ -9,7 +9,7 @@ import {
   listWrapperStyle,
 } from "@/illa-public-component/MemberList/components/List/style"
 import RoleSelect from "@/illa-public-component/RoleSelect"
-import { UsageCard } from "@/illa-public-component/UsageCard"
+// import { UsageCard } from "@/illa-public-component/UsageCard"
 import { USER_ROLE } from "@/illa-public-component/UserRoleUtils/interface"
 
 export const List: FC<ListProps> = (props) => {
@@ -153,7 +153,7 @@ export const List: FC<ListProps> = (props) => {
 
   return (
     <div css={listWrapperStyle}>
-      <UsageCard type={"License"} current={3} total={10} />
+      {/* <UsageCard type={"License"} current={3} total={10} /> */}
       {data?.length ? (
         <Table
           data={data}
