4f2e56b260c8a7da0a0466944386094ef708a430
Scenery <1507337624@qq.com>
2023-08-22 16:02:31 +0800

    feat: member invite mobile

---
+++ InviteModal/InviteMember/mobile/index.tsx
@@ -0,0 +1,60 @@
+import { FC } from "react"
+import { CloseIcon, Divider, Drawer, TriggerProvider } from "@illa-design/react"
+import { InviteByEmailMobile } from "../../component/InviteByEmail/mobile"
+import { InviteLinkMobile } from "../../component/InviteLink/mobile"
+import { InviteMemberProps } from "../interface"
+import {
+  closeIconContainerStyle,
+  dividerStyle,
+  inviteHeaderContainerStyle,
+  inviteModalStyle,
+} from "./style"
+
+export const ShareAgentMobile: FC<InviteMemberProps> = (props) => {
+  const { onClose } = props
+
+  return (
+    <TriggerProvider renderInBody zIndex={1005}>
+      <Drawer
+        _css={inviteModalStyle}
+        w="100%"
+        placement="bottom"
+        maskClosable={false}
+        closable={false}
+        footer={false}
+        onCancel={onClose}
+        visible={true}
+      >
+        <div css={inviteHeaderContainerStyle}>
+          <div
+            css={closeIconContainerStyle}
+            onClick={() => {
+              props.onClose?.()
+            }}
+          >
+            <CloseIcon size="12" />
+          </div>
+        </div>
+        <div>
+          <InviteLinkMobile
+            defaultInviteUserRole={props.defaultInviteUserRole}
+            defaultAllowInviteLink={props.defaultAllowInviteLink}
+            teamID={props.teamID}
+            currentUserRole={props.currentUserRole}
+            onInviteLinkStateChange={props.onInviteLinkStateChange}
+            onCopyInviteLink={props.onCopyInviteLink}
+            defaultBalance={props.defaultBalance}
+          />
+          <Divider _css={dividerStyle} />
+          <InviteByEmailMobile
+            defaultInviteUserRole={props.defaultInviteUserRole}
+            teamID={props.teamID}
+            currentUserRole={props.currentUserRole}
+            defaultBalance={props.defaultBalance}
+            onBalanceChange={props.onBalanceChange}
+          />
+        </div>
+      </Drawer>
+    </TriggerProvider>
+  )
+}
