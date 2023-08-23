7bef231bcf1a77da2fc917822e26d86d384c41bc
Scenery <1507337624@qq.com>
2023-08-23 15:06:05 +0800

    feat: fix invite url

--- InviteModal/utils/index.ts
+++ InviteModal/utils/index.ts
@@ -1,5 +1,5 @@
 export const getMarketLinkTemplate = (appID: string): string => {
-  return `${process.env.ILLA_MARKET_URL}/apps/${appID}/deploy`
+  return `${process.env.ILLA_MARKET_URL}/apps/${appID}/detail`
 }
 
 export const getPublicLinkTemplate = (teamIdentify: string, appID: string): string => {
