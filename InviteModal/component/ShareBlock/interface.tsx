cc79149e632c8954b4742d1d6f66234f8faca263
AruSeito <weichen@illasoft.com>
2023-08-25 19:09:35 +0800

    fix: 🐛 update file path

--- InviteModal/component/ShareBlock/interface.tsx
+++ InviteModal/component/ShareBlock/interface.tsx
@@ -1,11 +1,10 @@
 import { ReactNode } from "react"
 import { ReactComponent as FacebookIcon } from "../../asset/Facebook.svg"
-import { ReactComponent as HackerNewsIcon } from "../../asset/HackerNews.svg"
 import { ReactComponent as LinkedInIcon } from "../../asset/LinkedIn.svg"
 import { ReactComponent as RedditIcon } from "../../asset/Reddit.svg"
 import { ReactComponent as WhatsAppIcon } from "../../asset/Whatsapp.svg"
 import { ReactComponent as XIcon } from "../../asset/X.svg"
-
+import { ReactComponent as HackerNewsIcon } from "../../asset/hackernews.svg"
 
 export interface ShareBlockProps {
   shareUrl: string
