6ee28cee08e4a1036f41a9b0a09e549b9489f2a4
Scenery <1507337624@qq.com>
2023-08-25 10:19:00 +0800

    feat: add market app

---
+++ MarketApp/service/index.ts
@@ -0,0 +1,21 @@
+import { publicMarketplaceRequest } from "@illa-public/illa-net"
+import { AppProductList, PRODUCT_SORT_BY, ProductListParams } from "./interface"
+
+export const fetchAppList = (params: ProductListParams) => {
+  const {
+    page = 0,
+    limit = 10,
+    sortedBy = PRODUCT_SORT_BY.POPULAR,
+    search,
+  } = params
+  return publicMarketplaceRequest<AppProductList>({
+    url: "/apps",
+    method: "GET",
+    params: {
+      page,
+      limit,
+      sortedBy,
+      search,
+    },
+  })
+}
