23479126767a8e3353bfd0f2788eb74c456abf25
AruSeito <weichen@illasoft.com>
2023-08-29 17:26:09 +0800

    fix: 💄 update code editor style

--- CodeEditor/CodeMirror/style.ts
+++ CodeEditor/CodeMirror/style.ts
@@ -1,11 +1,11 @@
 import { css } from "@emotion/react"
 import { getColor } from "@illa-design/react"
 
-export const applyEditorWrapperStyle = css`
+export const applyEditorWrapperStyle = (editable: boolean) => css`
   width: 100%;
   height: 100%;
   .cm-editor {
-    border: 1px solid ${getColor("grayBlue", "08")};
+    border: ${editable ? `1px solid ${getColor("grayBlue", "08")}` : "none"};
     border-radius: 8px;
     transition: 0.2s ease-in-out;
     transition-property: border;
@@ -13,8 +13,13 @@ export const applyEditorWrapperStyle = css`
       z-index: 1;
       border: 1px solid ${getColor("techPurple", "01")} !important;
     }
+    .cm-line {
+      padding: 0 ${editable ? "16px" : "0"};
+    }
     :hover {
-      border: 1px solid ${getColor("techPurple", "06")};
+      border: ${editable
+        ? `1px solid ${getColor("techPurple", "06")}`
+        : "none"};
     }
   }
 `
