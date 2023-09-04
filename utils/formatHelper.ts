d8a5a93324d089dc48ef9620cc1d05f1ca556ce0
Scenery <1507337624@qq.com>
2023-09-04 17:37:48 +0800

    feat: update format function

--- utils/formatHelper.ts
+++ utils/formatHelper.ts
@@ -1,7 +1,6 @@
 export const formatNumForAgent = (num: number) => {
   if (typeof num !== "number") return "0"
-  if (num < 100) return `${num}`
-  if (num < 1000) return `${(num / 1000).toFixed(1)}k`
+  if (num < 1000) return `${num}`
   return `${(num / 1000).toFixed(1)}k`
 }
 
