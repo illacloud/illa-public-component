4874cb065d99b675ba3dd578359bd531d0b2b945
Scenery <1507337624@qq.com>
2023-09-22 19:18:57 +0800

    feat: add handle colla purchage error

--- UpgradeModal/utils.ts
+++ UpgradeModal/utils.ts
@@ -1,3 +1,7 @@
+import { ERROR_FLAG, ILLAApiError, isILLAAPiError } from "@illa-public/illa-net"
+import { createCollarModal, createPayErrorModal } from "./hook"
+import { CollarModalType } from "./interface"
+
 export function getSuccessRedirectWithParams(
   params: Record<string, string>,
 ): string {
@@ -10,4 +14,30 @@ export function getSuccessRedirectWithParams(
     .join("&")
 
   return `${process.env.ILLA_CLOUD_URL}${redirectPath}?${paramString}`
+}
+
+export const handleCollaPurchaseError = (
+  e: unknown,
+  modalType: CollarModalType,
+) => {
+  const collarModal = createCollarModal()
+  const payErrorModal = createPayErrorModal()
+  if (
+    isILLAAPiError(e) &&
+    e.data.errorFlag === ERROR_FLAG.ERROR_COLLA_UNSUBSCRIBE
+  ) {
+    collarModal?.({
+      modalType,
+    })
+    return true
+  } else if (
+    isILLAAPiError(e) &&
+    e.data.errorFlag === ERROR_FLAG.ERROR_COLLA_PAY_FAILED
+  ) {
+    payErrorModal?.({
+      modalType,
+    })
+    return true
+  }
+  return false
 }
