f3bfe441e66616ef18148bceeebbdfe7b903be3c
chenlongbo <chenlongbo@illasoft.com>
2023-10-16 20:57:29 +0800

    feat: add tag controller style

--- InviteModal/component/TagController/interface.ts
+++ InviteModal/component/TagController/interface.ts
@@ -1,6 +1,8 @@
 import { USER_ROLE } from "@illa-public/user-data"
 
 export interface TagControllerProps {
+  teamID: string
+  productID: string
   productType: ProductType
   userRoleForThisProduct: USER_ROLE
   onUpdateHashtags: (hashtags: string[]) => void
--- InviteModal/component/TagController/pc/index.tsx
+++ InviteModal/component/TagController/pc/index.tsx
@@ -1,7 +1,62 @@
-import { FC } from "react"
+import { USER_ROLE } from "@illa-public/user-data"
+import { isBiggerThanTargetRole } from "@illa-public/user-role-utils"
+import { FC, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
+import { Button, InputTag, getColor } from "@illa-design/react"
+import { updateContributeAttr } from "../../AgentToMarketplace/service"
 import { TagControllerProps } from "../interface"
+import { tagContainer, tagInputContainerStyle, titleStyle } from "./style"
 
 
 export const TagControllerPC: FC<TagControllerProps> = (props) => {
-  return <div></div>
+  const { userRoleForThisProduct, productType, productID, teamID } = props
+
+  const { t } = useTranslation()
+
+  const [currentHashTag, setCurrentHashTag] = useState<string[]>([])
+
+  const [saving, setSaving] = useState(false)
+
+  useEffect(() => {}, [productID, productType])
+
+  return (
+    <div css={tagContainer}>
+      <div css={titleStyle}>{t("Tag")}</div>
+      <div css={tagInputContainerStyle}>
+        <InputTag
+          labelInValue={false}
+          readOnly={
+            !isBiggerThanTargetRole(
+              USER_ROLE.VIEWER,
+              userRoleForThisProduct,
+              false,
+            )
+          }
+          saveOnBlur={true}
+          value={currentHashTag}
+          onChange={(value) => {
+            setCurrentHashTag(value as string[])
+          }}
+        />
+        <Button
+          ml="8px"
+          w="80px"
+          colorScheme={getColor("grayBlue", "02")}
+          loading={saving}
+          onClick={async () => {
+            setSaving(true)
+            try {
+              await updateContributeAttr(teamID, productID, currentHashTag)
+            } catch (e) {
+              console.error(e)
+            } finally {
+              setSaving(false)
+            }
+          }}
+        >
+          {t("Saving")}
+        </Button>
+      </div>
+    </div>
+  )
 }
