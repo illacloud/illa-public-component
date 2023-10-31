6d1db8d6aa8b332e92b3dbc8a3ffaba3b5cb8897
chenlongbo <chenlongbo@illasoft.com>
2023-10-31 23:22:51 +0800

    chore: remove gpt32k

--- MarketAgent/modelList.tsx
+++ MarketAgent/modelList.tsx
@@ -2,6 +2,7 @@ import { ReactComponent as MetaIcon } from "./assets/meta.svg"
 import { ReactComponent as OpenAIIcon } from "./assets/openai.svg"
 import { AI_AGENT_MODEL, LLM } from "./interface"
 
+
 export const getLLM = (type: AI_AGENT_MODEL): LLM | undefined => {
   return allModelList.find((item) => item.value === type)
 }
@@ -40,13 +41,6 @@ export const premiumModelList: LLM[] = [
     value: AI_AGENT_MODEL.GPT_4,
     logo: <OpenAIIcon title="GPT-4 Icon" />,
     limit: 8192,
-    temperatureRange: [0.1, 2],
-  },
-  {
-    name: "GPT-4-32k",
-    value: AI_AGENT_MODEL.GPT_4_32K,
-    logo: <OpenAIIcon title="GPT-4-32k Icon" />,
-    limit: 32000,
     temperatureRange: [0.1, 2],
   },
   {
