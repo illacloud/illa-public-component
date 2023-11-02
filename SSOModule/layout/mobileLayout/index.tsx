b8fd575ad7081371bc370129de49cccf36803618
Scenery <1507337624@qq.com>
2023-11-02 21:18:28 +0800

    feat: update privacy link

--- SSOModule/layout/mobileLayout/index.tsx
+++ SSOModule/layout/mobileLayout/index.tsx
@@ -2,6 +2,7 @@ import {
   ILLA_MIXPANEL_EVENT_TYPE,
   MixpanelTrackContext,
 } from "@illa-public/mixpanel-utils"
+import { DOC_PREFIX } from "@illa-public/public-configs"
 import { TextLink } from "@illa-public/text-link"
 import { isCloudVersion } from "@illa-public/utils"
 import { FC, useContext } from "react"
@@ -18,7 +19,7 @@ export const MobileUserLayout: FC<LayoutProps> = ({ children }) => {
       track?.(ILLA_MIXPANEL_EVENT_TYPE.CLICK, {
         element: /privacy/.test(link) ? "privacy" : "terms",
       })
-      window.open("https://illacloud.com/docs" + link, "_blank")
+      window.open(DOC_PREFIX + link, "_blank")
     } else {
       window.open(link, "_blank")
     }
