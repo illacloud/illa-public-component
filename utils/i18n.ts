a2477e964e470d19024d25685690a9fd8820d4e9
Scenery <1507337624@qq.com>
2023-11-13 10:23:30 +0800

    revert: i18n

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,4 +1,25 @@
-export const languageKeys = ["en-US", "zh-CN", "ja-JP", "de-DE"]
+export const languageKeys = [
+  "en-US",
+  "zh-CN",
+  "ja-JP",
+  "ko-KR",
+  "cs-CZ",
+  "da-DK",
+  "de-DE",
+  "el-GR",
+  "es-ES",
+  "fi-FI",
+  "fr-FR",
+  "it-IT",
+  "nl-NL",
+  "no-NO",
+  "pl-PL",
+  "pt-PT",
+  "ru-RU",
+  "ro-RO",
+  "sv-SE",
+  "uk-UA",
+]
 
 export const formatLanguage = (code: string) => {
   if (code) {
