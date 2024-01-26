47040fee4cb6c43c0cb35fb8e6abb065e2b92801
Scenery <1507337624@qq.com>
2024-01-26 13:52:07 +0800

    feat: add create app mixpanel oboarding params

--- CreateAppModal/constants.ts
+++ CreateAppModal/constants.ts
@@ -1,11 +1,13 @@
 export enum REPORT_PARAMETER {
   BLANK_DASHBOARD = "blank_dashboard",
   BLANK_APP = "blank_app",
+  BLANK_TUTORIAL_APP = "tutorial_app",
   CREATE_APP_MODAL = "create_app_modal",
   MORE_TEMPLATE = "more_template",
   BLANK_DASHBOARD_CREATE = "blank_dashboard_create",
   DASHBOARD_CREATE_NEW = "dashboard_create_new",
   BLANK_APP_CREATE = "blank_app_create",
+  TUTORIAL_APP_CREATE = "tutorial_app_create",
 }
 
 export enum REPORT_TEMPLATE_STATUS {
